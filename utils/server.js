849c6d4af3b7ccf0281882325a425c829a5a6f88
陈泳瑾 <chenyj@chinatelecom.cn>
2018-06-21 18:35:56 +0800

    change server

--- utils/server.js
+++ utils/server.js
@@ -1,4 +1,8 @@
 var  serverConfig = {
-       DB_CONN_STR:'mongodb://47.98.47.153:27017/SchulteGrid'
+       DB_CONN_STR:'mongodb://47.98.47.153:27017/SchulteGrid',
+       server: {
+        auto_reconnect: true,
+        poolSize: 10
+    }
 }
 export default serverConfig;

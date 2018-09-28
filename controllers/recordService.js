a15920ad3a9bdb4af929dfa1c30077c10caf9764
liwenbin <837531387@qq.com>
2018-09-28 13:41:13 +0800

    限制排行 前100条

--- controllers/recordService.js
+++ controllers/recordService.js
@@ -1,4 +1,7 @@
-import { errorWrapper, successWrapper } from '../utils/utils.js';
+import {
+  errorWrapper,
+  successWrapper
+} from '../utils/utils.js';
 import Record from '../models/record';
 import Token from '../models/token';
 import config from '../appConfig.json';
@@ -11,9 +14,19 @@ const fs = require('fs');
 export const saveRecord = async (ctx, next) => {
   ctx.set('Cache-Control', 'no-cache');
   let request = ctx.request.body;
-  let { record, nickName, openId, avatarUrl, type } = request;
-  let res = await Record.find({ openId });
-  let postData = { ...request, [`record_type${type}`]: record };
+  let {
+    record,
+    nickName,
+    openId,
+    avatarUrl,
+    type
+  } = request;
+  let res = await Record.find({
+    openId
+  });
+  let postData = { ...request,
+    [`record_type${type}`]: record
+  };
   let result;
   // 判断是否有记录
   if (res.length) {
@@ -22,7 +35,9 @@ export const saveRecord = async (ctx, next) => {
     if (currentRecord && currentRecord < record) {
       result = '当前为最好成绩'
     } else {
-      result = await Record.update({ openId }, postData).then(data => {
+      result = await Record.update({
+        openId
+      }, postData).then(data => {
         return '更新成功'
       })
     }
@@ -38,14 +53,24 @@ export const saveRecord = async (ctx, next) => {
 export const getRecord = async (ctx, next) => {
   ctx.set('Cache-Control', 'no-cache');
   let request = ctx.params;
-  let { openId, type } = request;
-  let res = await Record.find({ openId });
+  let {
+    openId,
+    type
+  } = request;
+  let res = await Record.find({
+    openId
+  });
   ctx.response.body = successWrapper(res.length ? res[0][`record_type${type}`] : 0);
 }
 
 export const login = async (ctx, next) => {
-  let { code } = ctx.request.body;
-  let { AppId, AppSecret } = config;
+  let {
+    code
+  } = ctx.request.body;
+  let {
+    AppId,
+    AppSecret
+  } = config;
   var options = {
     method: 'GET',
     uri: `https://api.weixin.qq.com/sns/jscode2session?appid=${AppId}&secret=${AppSecret}&js_code=${code}&grant_type=authorization_code`,
@@ -56,21 +81,24 @@ export const login = async (ctx, next) => {
 }
 
 export const getMiniProgramQrcode = async (ctx, next) => {
-  let { AppId, AppSecret } = config;
+  let {
+    AppId,
+    AppSecret
+  } = config;
   let access_token = '';
   let date = new Date();
   // 查找是否生成过token
   let tokens = await Token.find({});
   let currentToken = tokens[0];
   // 验证access_token是否失效
-  if(currentToken && (date.getTime()-currentToken.save_time)/1000 < currentToken.expires_in) {
+  if (currentToken && (date.getTime() - currentToken.save_time) / 1000 < currentToken.expires_in) {
     access_token = currentToken.access_token;
   } else {
     // 失效先清空token
-    if(currentToken) {
+    if (currentToken) {
       await Token.remove({})
     }
-    
+
     // 重新获取access_token
     var options = {
       method: 'GET',
@@ -78,7 +106,9 @@ export const getMiniProgramQrcode = async (ctx, next) => {
       json: true
     };
     let result = await request(options);
-    let token = new Token({...result, save_time: date.getTime()});
+    let token = new Token({ ...result,
+      save_time: date.getTime()
+    });
     await token.save();
     access_token = result.access_token;
     let options1 = {
@@ -93,30 +123,42 @@ export const getMiniProgramQrcode = async (ctx, next) => {
     // 获取二维码并保存
     let result1 = await request(options1).pipe(fs.createWriteStream('/usr/etc/miniprogramqrcode.png'));
   }
-  
-  let fileUrl = 'https://' + ctx.headers.host + '/miniprogramqrcode.png';    
+
+  let fileUrl = 'https://' + ctx.headers.host + '/miniprogramqrcode.png';
   ctx.response.body = successWrapper(fileUrl);
 }
 
 export const getList = async (ctx, next) => {
   let request = ctx.params;
-  let { type } = request;
+  let {
+    type
+  } = request;
   let res = [];
 
   await Record.find({
-      [`record_type${type}`]: { $gt: 0 }
-    }).sort({
+      [`record_type${type}`]: {
+        $gt: 0
+      }
+    }).limit(100).sort({
       [`record_type${type}`]: 1
     }).lean()
     .then(
       (docs) => {
         docs.map((data, key) => {
-          let { openId, nickName, avatarUrl } = data;
-          res.push({ openId, nickName, avatarUrl, record: data[`record_type${type}`] , number: key + 1})
+          let {
+            openId,
+            nickName,
+            avatarUrl
+          } = data;
+          res.push({
+            openId,
+            nickName,
+            avatarUrl,
+            record: data[`record_type${type}`],
+            number: key + 1
+          })
         })
       }
     )
   ctx.response.body = successWrapper(res);
 }
-
-
--- routers/msg.js
+++ routers/msg.js
@@ -1,7 +1,14 @@
 const msgRoute = (router) => {
     router.get('/msg', function (ctx, next) {
-        let request = ctx.query;
-        console.log(request);
+        let query = ctx.query;
+        console.log(query);
+        ctx.response.body = request.echostr
+    });
+
+
+    router.post('/msg', function (ctx, next) {
+        let body = ctx.body;
+        console.log(body);
         ctx.response.body = request.echostr
     });
 }
